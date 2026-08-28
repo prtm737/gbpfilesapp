dddbcfcf22cee98308874e629e159f7f63452c1d
gbp-ftms <admin@gbp.local>
2026-08-28 19:47:29 +0530

    deploy: update gh-pages with latest production Vite build and SPA routing fixes

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gbpfilesapp/sw.js', { scope: '/gbpfilesapp/' })})}
--- sw.js
+++ sw.js
@@ -1,35 +1,114 @@
-const CACHE_NAME = 'gbp-ftms-v1'
-const CORE_ASSETS = ['/', '/index.html']
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)).then(() => self.skipWaiting())
-  )
-})
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  )
-})
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
 
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url)
-  if (event.request.method !== 'GET') return
-  // Never cache Supabase API calls
-  if (url.hostname.includes('supabase')) return
-
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone()
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(() => {})
-        return response
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
       })
-      .catch(() =>
-        caches.match(event.request).then(cached => cached || caches.match('/index.html'))
-      )
-  )
-})
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-290dd570'], (function (workbox) { 'use strict';
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+  workbox.precacheAndRoute([{
+    "url": "registerSW.js",
+    "revision": "482580d6f57d0ff733fbdc9db18c93d8"
+  }, {
+    "url": "index.html",
+    "revision": "948b7f777a54da54ed12ef9fe0291f63"
+  }, {
+    "url": "icons.svg",
+    "revision": "3b4fcfcf393eca4d264dca4a4663bc37"
+  }, {
+    "url": "favicon.svg",
+    "revision": "665f90e2670091b486b13996d66d0d31"
+  }, {
+    "url": "assets/index-C4opb0P9.js",
+    "revision": null
+  }, {
+    "url": "assets/index-9xTsCfGc.css",
+    "revision": null
+  }, {
+    "url": "favicon.svg",
+    "revision": "665f90e2670091b486b13996d66d0d31"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "6c8be2eaec9dff599ed4e9c1c9dea6ba"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+  workbox.registerRoute(/^https?:\/\/.*\/rest\/.*/i, new workbox.NetworkFirst({
+    "cacheName": "supabase-cache",
+    plugins: [new workbox.ExpirationPlugin({
+      maxEntries: 50,
+      maxAgeSeconds: 300
+    })]
+  }), 'GET');
+
+}));
